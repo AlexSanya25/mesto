b5079cb221fcb53783cbefdcb8a5dba1c7ef7b6f
Alexandr Gavrik <sgavrik03@yandex.ru>
2023-07-12 02:42:07 +0300

    fixxx

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,31 @@
 const popup = document.querySelector('.popup');
-const popupOpenButton = document.querySelector('.profile__editbutton_open-popup');
+const popupOpenButton = document.querySelector('.profile__editbutton');
 const popupCloseButton = popup.querySelector('.popup__close');
 const formElement = document.querySelector('.popup__input-container');
-const formSaveButton = popup.querySelector('.popup__save');
 
-let Name = document.querySelector('.profile__title_name');
-let Job = document.querySelector('.profile__subtitle_job');
-let NameInput = popup.querySelector('.popup__input-name');
-let JobInput = popup.querySelector('.popup__input-job');
+
+let name = document.querySelector('.profile__title');
+let job = document.querySelector('.profile__subtitle');
+let nameInput = popup.querySelector('.popup__input_name');
+let jobInput = popup.querySelector('.popup__input_job');
 
 const popupToggle = function () {
     popup.classList.toggle('popup_opened');
-    NameInput.value = Name.textContent;
-    JobInput.value = Job.textContent;
+    nameInput.value = name.textContent;
+    jobInput.value = job.textContent;
 }
-popupOpenButton.addEventListener('click', popupToggle);
 
 function handleFormSubmit(evt) {
     evt.preventDefault();
-    let NameValue = NameInput.value;
-    let JobValue = JobInput.value;
-    Name.textContent = NameValue;
-    Job.textContent = JobValue;
+    let nameValue = nameInput.value;
+    let jobValue = jobInput.value;
+    name.textContent = nameValue;
+    job.textContent = jobValue;
+    popupToggle();
 };
 
+popupOpenButton.addEventListener('click', popupToggle);
 popupCloseButton.addEventListener('click', function () {
     popup.classList.toggle('popup_opened');
 });
-
-formSaveButton.addEventListener('click', function () {
-    popup.classList.toggle('popup_opened');
-});
-
 formElement.addEventListener('submit', handleFormSubmit);
